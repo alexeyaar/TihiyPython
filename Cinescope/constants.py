41cdea94aa50de43dfe7abd465b739c08810aa4e
Alexey Artemov <alexey.rk.tinkoff@gmail.com>
2026-02-04 19:52:22 +0300

    add kotyata

---
+++ Cinescope/constants.py
@@ -0,0 +1,7 @@
+URL_MOVIE = BASE_URL = "https://auth.dev-cinescope.coconutqa.ru"
+HEADERS = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
+LOGIN_ENDPOINT = "/login"
+REGISTER_ENDPOINT = "/register"
